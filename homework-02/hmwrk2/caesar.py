266f767d14371782d20dd02f7314751689ce5792
Воробчук Лилия <115791216+lavorobchuk@users.noreply.github.com>
2024-02-29 22:05:09 +0300

    Add files via upload

---
+++ homework-02/hmwrk2/caesar.py
@@ -0,0 +1,75 @@
+import typing as tp
+
+
+def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
+    """
+    Encrypts plaintext using a Caesar cipher.
+
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+    ciphertext = ""
+    name_lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                  'u', 'v', 'w', 'x', 'y', 'z']
+    name_upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                  'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    for i in plaintext:
+        if i in name_lower:
+            ciphertext += name_lower[(name_lower.index(i) + shift) % 26]
+        elif i in name_upper:
+            ciphertext += name_upper[(name_upper.index(i) + shift) % 26]
+        else:
+            ciphertext += i
+
+    return ciphertext
+
+
+def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
+    """
+    Decrypts a ciphertext using a Caesar cipher.
+
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
+    plaintext = ""
+    name_lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                  'u', 'v', 'w', 'x', 'y', 'z']
+    name_upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                  'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    for i in ciphertext:
+        if i in name_lower:
+            plaintext += name_lower[(name_lower.index(i) - shift) % 26]
+        elif i in name_upper:
+            plaintext += name_upper[(name_upper.index(i) - shift) % 26]
+        else:
+            plaintext += i
+    return plaintext
+
+
+def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
+    """
+    Brute force breaking a Caesar cipher.
+    """
+    best_shift = 0
+
+    for i in range(0, 26):
+        new = decrypt_caesar(ciphertext, i)
+        if new in dictionary:
+            best_shift = i
+            break
+
+    return best_shift
